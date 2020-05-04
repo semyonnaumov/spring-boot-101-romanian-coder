8ee5e64cd3a59795872f3dae2eea0c9564d2037a
Semyon Naumov <Naumov.S.Serg@sberbank.ru>
2020-05-05 00:08:30 +0300

    Add more sophisticated things in properties

--- src/main/java/com/naumov/booking/ViewController.java
+++ src/main/java/com/naumov/booking/ViewController.java
@@ -1,5 +1,8 @@
 package com.naumov.booking;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +15,14 @@ import java.util.Date;
 @Controller
 public class ViewController {
 
+    //@Value("${app-mode}") // Binds the property app-mode to this field. (Constructor injection is more preferable)
+    private String appMode;
+
+    @Autowired
+    public ViewController(Environment environment) {
+        appMode = environment.getProperty("app-mode"); //Constructor injection of property
+    }
+
     /**
      * Simple controller method.
      * @return the name of a view (html file) from ./resources/templates. The output is processed by Thymeleaf using given {@Param model}.
@@ -20,7 +31,7 @@ public class ViewController {
     public String index(Model model) {
         model.addAttribute("datetime", new Date());
         model.addAttribute("username", "Kolyan Sucks");
-        model.addAttribute("mode", "production");
+        model.addAttribute("mode", appMode);
         return "index";
     }
 }
